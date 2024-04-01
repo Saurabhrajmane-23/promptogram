5873b5bb6e04ea9cc856ddceae7a0ec60708a19c
Saurabhrajmane-23 <iamsaurabhrajmane@gmail.com>
2024-04-01 12:34:59 +0530

    changed next-config.js

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 import Form from "@components/Form";
 
